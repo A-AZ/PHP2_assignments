390e974553b0c4aecfabe424d44e9fd03332f24c
A-AZ <115350696+A-AZ@users.noreply.github.com>
2022-10-30 12:33:03 +0300

    Update m.4_partyScript.js

--- m.4_partyScript/m.4_partyScript.js
+++ m.4_partyScript/m.4_partyScript.js
@@ -7,15 +7,13 @@ do {
     userAge = parseInt(userAge);
     if (isNaN(userAge)) {
         continue; }
-}
-if (userAge < minAge) {
-    displayMsg("You are too young");
-} else if (userAge > 35) {
-    displayMsg ("You are too old");
-} else {
-    displayMsg ("Hello " + prompt("What's is your Name?") + ", Welcome to the party."); 
+    
+    if (userAge < minAge) {
+    displayMsg("You are too young");}
+    else if (userAge > 35) {
+    displayMsg ("You are too old");}
+    else {
+    displayMsg ("Hello " + prompt("What's is your Name?") + ", Welcome to the party."); } 
 } while (confirm("is there anyone else?"));
-
 function displayMsg(msg) {
-  alert(msg);
-}
+  alert(msg);}
